3f69effd19eae4bd27caf963199394fb817dc81c
Oussama Benoujja <osama2code79@gmail.com>
2025-02-18 20:03:47 +0100

    Update Mailer.php

--- app/utils/Mailer.php
+++ app/utils/Mailer.php
@@ -17,7 +17,7 @@ class Mailer {
             $mail->Host       = 'smtp.gmail.com';        
             $mail->SMTPAuth   = true;
             $mail->Username   = 'osama2code79@gmail.com';   
-            $mail->Password   = 'xnbq cqcr albs hpmw';         
+            $mail->Password   = '';         
             $mail->SMTPSecure = 'tls';                    
             $mail->Port       = 587;                     
 
